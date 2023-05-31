17b5fb1528cd7925c8871ff68348c043c1083284
test <test@test>
2023-05-31 06:34:30 +0200

    ajout numero et hidden btn sur img

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -2,19 +2,28 @@ import imgPrev from '../../assets/img/previous.svg'
 import imgNext from '../../assets/img/next.svg'
 import { useState } from 'react';
 const Slideshow = ({ img }) => {
-    
+
     const [index, setNext] = useState(0)
+    console.log(img.length)
     return (
         <div className='slideshow'>
-            <button onClick={() => setNext(index <= 0 ? img.length -2: index - 1)} className='slideshow__prev'>
-                <img src={imgPrev} alt="" />
+
+            <button onClick={() => setNext(index <= 0 ? img.length - 2 : index - 1)} className={`slideshow__prev ${img.length < 2 ? "slideshow__hidden" : ""}`}>
+                <img src={imgPrev} alt="previous" />
             </button>
-            <img className='slideshow__img' src={img[index]} alt="cover house" />
-            <button onClick={() => {setNext(index > img.length - 2 ? 0 : index + 1)}} className='slideshow__next'>
-                <img src={imgNext} alt="" />
+
+            <div className='slideshow__img-container'>
+                <img className='slideshow__img' src={img[index]} alt="cover house" />
+                <span className={`slideshow__img-number ${img.length < 2 ? "slideshow__hidden" : ""}`}>{index + 1}</span>
+            </div>
+
+            <button onClick={() => { setNext(index > img.length - 2 ? 0 : index + 1) }} className={`slideshow__next ${img.length < 2 ? "slideshow__hidden" : ""}`}>
+                <img src={imgNext} alt="next" />
             </button>
         </div>
     );
 };
 
 export default Slideshow;
+
+
